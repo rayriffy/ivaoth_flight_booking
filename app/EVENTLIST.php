2efadd49c3f2dc97167af72e7f7fdda93f295a01
Phumrapee Limpianchop <rayriffy@gmail.com>
2018-05-31 13:52:04 +0700

    🚨 Intitialize

---
+++ app/EVENTLIST.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EVENTLIST extends Model
+{
+    //
+
+    protected $table = 'eventlist';
+    
+    protected $primaryKey = 'id';
+}
--- script/conn.php
+++
@@ -1,4 +0,0 @@
-<?
-  mysql_connect('#HOST#','#USER#','#PASS#') or die('ERR: Could not connect to MySQL');
-  mysql_select_db('ivaoth_book') or die('ERR: Database not found');
-?>
